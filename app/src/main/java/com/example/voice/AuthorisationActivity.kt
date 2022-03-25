fbed090096c7356071e9462c691a191849ad332d
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2022-03-25 23:13:53 +0300

    first1

---
+++ app/src/main/java/com/example/voice/AuthorisationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.voice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorisationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorisation)
+
+    }
+}
