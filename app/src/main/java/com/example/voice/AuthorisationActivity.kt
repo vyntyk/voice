09a9548dca2c9ee3f3e3bb93807b6065b7e97f56
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2022-03-26 16:52:32 +0300

    1

--- app/src/main/java/com/example/voice/AuthorisationActivity.kt
+++ app/src/main/java/com/example/voice/AuthorisationActivity.kt
@@ -1,12 +1,48 @@
 package com.example.voice
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.ui.VKConfirmationActivity.Companion.result
+import java.lang.Exception
 
 class AuthorisationActivity : AppCompatActivity() {
+
+    val authLauncher = VK.login(this) { result : VKAuthenticationResult ->
+        when (result) {
+            is VKAuthenticationResult.Success -> onLogin()
+            is VKAuthenticationResult.Failed -> onLoginFailed(result.exception)
+        }
+    }
+
+    fun onLogin(){
+        Toast.makeText(this, "Успешно",Toast.LENGTH_SHORT).show()
+    }
+
+    fun onLoginFailed(exception: Exception){
+        Toast.makeText(this, "Ошибка",Toast.LENGTH_SHORT).show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorisation)
 
+    }
+
+    val loginBtn = findViewById<Button>(R.id.btnVKlogin)
+    loginBtn.setOnClickListener {
+        authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+    }
+
+    companion object {
+        fun start(context: Context){
+            context.startActivity(Intent(context, AuthorisationActivity::class.java))
+        }
     }
 }
